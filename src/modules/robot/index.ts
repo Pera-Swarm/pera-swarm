31a533d4e84c53b2d2a51ee7757e46254ae8c7cc
Ganindu <ganindudananja@gmail.com>
2020-11-28 02:01:08 +0530

    Robot base module update (correct one)

--- src/modules/robot/index.ts
+++ src/modules/robot/index.ts
@@ -1,1 +1,45 @@
+/**
+ * @class Robot
+ * @classdesc Generic Robot class prototype
+ */
+abstract class Robot<
+    TId,
+    TCoordinate,
+    TCoordinateValueType,
+    TSensors,
+    TSensorsValueType
+> {
+    protected _id: TId;
+    protected _coordinates: TCoordinate;
+    protected _sensors: TSensors;
+    protected _updated: number;
+
+    constructor(id: TId, coordinates: TCoordinate, sensors: TSensors) {
+        this._id = id;
+        this._coordinates = coordinates;
+        this._sensors = sensors;
+        this._updated = Date.now();
+    }
+
+    /**
+     * the robot id
+     */
+    get id() {
+        return this._id;
+    }
+
+    /**
+     * the robot updated
+     */
+    get updated() {
+        return this._id;
+    }
+
+    abstract get coordinates(): TCoordinateValueType;
+    abstract setCoordinates(coordinates: TCoordinateValueType): void;
+    abstract get sensors(): TSensorsValueType;
+    abstract setSensors(sensors: TSensorsValueType): void;
+}
+
 export * from './robot';
+export { Robot };
